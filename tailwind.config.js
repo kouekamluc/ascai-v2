afbf7e2f79655a9e396675294d437fe7f1752f9d
kouekam <kouekamkamgouluc@gmail.com>
2026-04-11 18:16:36 +0200

    new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
           green: '#007A5E',
           red: '#CE1126',
           yellow: '#FCD116',
-        }
+        },
+        ascai: {
+          ink: '#0f2e28',
+          footer: '#0c2420',
+          'green-soft': 'rgba(0, 122, 94, 0.06)',
+        },
       }
     }
   }
